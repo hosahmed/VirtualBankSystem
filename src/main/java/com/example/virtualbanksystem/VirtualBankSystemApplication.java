fed4f8dcc8d19d36501796c0b1a4c5fabd602d6e
Hossam Ahmed <hossamahmedmohmed2892004@gmail.com>
2026-07-19 11:36:58 +0300

    Initial Commit

---
+++ src/main/java/com/example/virtualbanksystem/VirtualBankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualbanksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBankSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualBankSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/virtualbanksystem/VirtualBankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualbanksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualBankSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
